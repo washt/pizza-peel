d71b339c26e966c991b0704b7440682d080c5672
Tucker Wash <tucker@songbird.sh>
2022-04-28 16:41:28 -0400

    preprocess data for area calculation

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,6 @@
 from functools import partial
 import fiona
+import json
 import pyproj
 from shapely.geometry import Point, Polygon
 from shapely.ops import transform
@@ -8,13 +9,12 @@ all_bars = []
 
 with fiona.open("data/osm-bars/bigquery-public-data-geo-openstreetmap-bars-point.shp") as bars:
   for bar in bars: 
-    _id = bar.get('id') 
-    coords = bar['geometry']['coordinates']
     name = bar['properties']['name']
     address = bar['properties']['address']
     poi_type = bar['properties']['poi_type']
+    coords = bar['geometry']['coordinates']
 
-    all_bars.append([_id, name, poi_type, coords, address])
+    all_bars.append({"name": name,"address": address, "poi_type": poi_type, "coords": coords, "area": ""})
 
 footprint_polygons = []
 
@@ -23,15 +23,29 @@ with fiona.open("data/ms-bldg-footprints--dc.geojson") as footprints:
     geometry = footprint.get('geometry').get('coordinates')
     footprint_polygons.append(Polygon(geometry[0]))
 
-# Geodetic transform, borrowed from
-# https://gist.github.com/robinkraft/c6de2f988c9d3f01af3c
+# Geodetic transform to calculate area in meters^2
+# borrowed from https://gist.github.com/robinkraft/c6de2f988c9d3f01af3c
 projection = partial(pyproj.transform, pyproj.Proj(init='epsg:4326'),
                pyproj.Proj(init='epsg:3857'))
-
+count = 0
 for bar in all_bars:
   for footprint in footprint_polygons:
-    bar_point = Point(bar[3])
+    bar_point = Point(bar["coords"])
 
     if bar_point.within(footprint):
       transformed = transform(projection, footprint)
-      bar.append(transformed.area)
+      bar["area"] = transformed.area
+      print("Processed", count, "of", len(all_bars))
+      count += 1
+
+print("Writing to file..")
+
+with open('data/joined_data.json', 'w') as outfile:
+    outfile.write('[')
+    for bar in all_bars:
+        json.dump(bar, outfile)
+        if bar != all_bars[-1]:
+            outfile.write(',')
+    outfile.write(']')
+
+print('Complete!')
