5b61ba3ca954c76e13378b1980d4b146525bd293
Tucker Wash <tucker@songbird.sh>
2022-04-28 17:27:57 -0400

    nearest bars endpoint

--- server.py
+++ server.py
@@ -1,7 +1,38 @@
-from flask import Flask
+import json
+import numpy as np
+from scipy.spatial import KDTree
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+sorted_bars = []
+sorted_coords = []
+
+with open('data/joined_data.json') as joined_data:
+    bars = json.load(joined_data)
+
+    for bar in bars:
+      sorted_bars.append([bar.get('name'), bar.get('coords'), bar.get('area')])
+
+# sort bars by coordinates for result lookup
+sorted(sorted_bars, key=lambda k: [k[1][0], k[1][1]])
+
+# separate list of only coordinates for KDTree
+for bar in sorted_bars:
+    sorted_coords.append(bar[1])
+
+# KDTree gives us O(log n) lookup for nearest neighbors
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.KDTree.html
+tree = KDTree(np.array(sorted_coords))
+
+@app.route("/nearest_bars/", methods=["POST"])
+def nearest_bars():
+    req = request.get_json()
+    _lat = float(req.get('lat'))
+    _long = float(req.get('long'))
+
+    # query kd tree for 5 closest neighbors
+    _ , neighbor_indecies = tree.query([_lat, _long], k=5)
+
+    # look up our indecies in our original list to build result
+    return jsonify([sorted_bars[idx] for idx in neighbor_indecies])
